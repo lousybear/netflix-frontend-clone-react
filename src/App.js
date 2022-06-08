8c2d0ea1a7e446b7d5b7612ee70e786556ab5be2
Sagarbiswal <sagarbiswal569@gmail.com>
2022-06-08 08:37:05 +0530

    added movie rows

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Row from "./Row";
+import request from "./request";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Row title="Netflix Originals" fetchUrl={request.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+    </div>
+  );
 }
 
 export default App;
