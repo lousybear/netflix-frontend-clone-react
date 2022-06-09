a869e0c19101f04802e7e76f6f4685ced2a00ffd
Sagarbiswal <sagarbiswal569@gmail.com>
2022-06-09 09:02:39 +0530

    added css for flex and transition

--- src/Row.js
+++ src/Row.js
@@ -1,35 +1,36 @@
-import React, { useState, useEffect } from "react";
-import axios from "./axios";
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import './Row.css';
 
-const baseURL = "https://image.tmdb.org/t/p/original/";
+const baseURL = 'https://image.tmdb.org/t/p/original/';
 
 function Row({ title, fetchUrl }) {
-  const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      const request = await axios.get(fetchUrl);
-      setMovies(request.data.results);
-      return request;
-    }
-    fetchData();
-  }, [fetchUrl]);
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
 
-  return (
-    <div>
-      <h2>{title}</h2>
+    return (
+        <div className='row'>
+            <h2>{title}</h2>
 
-      <div className="row_posters">
-        {movies.map((movie) => (
-          <img
-            className="row_poster"
-            src={`${baseURL}${movie.poster_path}`}
-            alt={movie.name}
-          />
-        ))}
-      </div>
-    </div>
-  );
+            <div className='row_posters'>
+                {movies.map((movie) => (
+                    <img
+                        className='row_poster'
+                        src={`${baseURL}${movie.poster_path}`}
+                        alt={movie.name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Row;
